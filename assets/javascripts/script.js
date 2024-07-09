825e33a686ee197d8dc1b30c4c9d15dcc67a1af9
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-07-09 23:21:16 +0530

    All Script Completed just lap clearing logic is remaining.

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,33 +1,90 @@
 const playBtn = document.getElementsByClassName('m-btn')[0];
-const stopBtn = document.getElementsByClassName('re-btn')[0];
-const hours = document.getElementsByClassName('hrs')[0];
-const minutes = document.getElementsByClassName('mil')[0];
-const seconds = document.getElementsByClassName('sec')[0];
+const resetBtn = document.getElementsByClassName('re-btn')[0];
+const lapBtn = document.getElementsByClassName('st-btn')[0];
+const display = document.getElementsByClassName("display")[0];
+const lapTable = document.getElementsByClassName("lap-table")[0];
+const lapClearBtn = document.getElementsByClassName("lap-btn")[0];
 
 let flag = 0;
-let secCount = 0;
-let minCount = 0;
-let hrsCount = 0;
-let sec, min, hrs;
-
-if(sec == 60) sec = 0;
-if(sec == 60) sec = 0;
-if(sec == 60) sec = 0;
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let lapCount = 0;
 
 playBtn.addEventListener("click", ()=>{
     if(flag === 0){
-        playBtn.textContent = "Reset";
-        stopBtn.classList.remove("cover");
-        start(1);
+        playBtn.innerHTML = `<i class="ri-pause-line"></i>`;
+        resetBtn.classList.remove("cover");
+        lapBtn.classList.remove("cover");
+        start();
         flag = 1;
     } else{
-        playBtn.textContent = "Start";
-        stopBtn.classList.add("cover");
-        start(0);
+        playBtn.innerHTML = `<i class="ri-play-line"></i>`;
+        stop();
         flag = 0;
     }
 });
-stopBtn.addEventListener("click", ()=>{
+resetBtn.addEventListener("click", ()=>{
+    clearInterval(timer);
+    flag = 0;
+    startTime = 0;
+    elapsedTime = 0;
+    lapBtn.classList.add("cover");
+    resetBtn.classList.add("cover");
+    playBtn.innerHTML = `<i class="ri-play-line"></i>`;
+    lapCount = 0;
+    lapTable.innerHTML = `<div class="lap-head">
+                            <span>Index</span>
+                            <span>Timestamp</span>
+                        </div>
+                        <button class="lap-btn cover">Clear Laps</button>
+    `;
+    display.textContent = `00 : 00 : 00 : 00`;
 })
+lapBtn.addEventListener("click", ()=>{
+    lapCount++;
+        
+    let clutter = `<li class="lap-item">
+                        <span class="index"> LAP ${lapCount}</span>
+                        <span class="timestamp">${display.textContent}</span>
+                    </li>`
+    lapTable.innerHTML += clutter;
+})
+lapClearBtn.addEventListener("click", ()=>{
+    lapCount = 0;
+    lapTable.innerHTML = `
+    `
+    alert("Clicked");
+})
+
+function start(){
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+}
+
+function stop(){
+    clearInterval(timer);
+    elapsedTime = Date.now() - startTime;
+}
+
+function update(){
+    const currentTime  = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours} : ${minutes} : ${seconds} : ${milliseconds}`;
+}
+
+
+
 
 
